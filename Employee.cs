ff5ecde40e5818a3d0ef22fb63663980fe74389d
triumphantlyoptimistically <24ds0790@barton.ac.uk>
2026-02-10 16:33:25 +0000

    Put rota on Github

---
+++ Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rota
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Position { get; set; }
+        public List<Availability> AvailabilitySlots { get; set; }
+
+        public struct Availability
+        {
+            public DayOfWeek Day { get; }
+            public TimeSpan StartTime { get; }
+            public TimeSpan EndTime { get; }
+
+            public Availability(DayOfWeek day, TimeSpan startTime, TimeSpan endTime)
+            {
+                Day = day;
+                StartTime = startTime;
+                EndTime = endTime;
+            }
+
+            public override string ToString()
+            {
+                return $"{Day} {StartTime} - {EndTime}";
+            }
+        }
+
+        public Employee(int employeeID, string employeeName, string position, List<Availability> availabilitySlots)
+        {
+            EmployeeID = employeeID;
+            EmployeeName = employeeName;
+            Position = position;
+            AvailabilitySlots = availabilitySlots;
+        }
+
+        public override string ToString()
+        {
+            string employeeDetails = $"Employee: {EmployeeID}, {EmployeeName}, {Position}, ";
+            string availabilityString = string.Join(", ", AvailabilitySlots);
+            employeeDetails += availabilityString;
+            return employeeDetails;
+        }
+    }
+}
---
+++ Shift.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rota
+{
+    public class Shift
+    {
+        public int ShiftID { get; set; }
+        public Employee EmployeeOnShift { get; set; }
+        public string Position { get; set; }
+        public DateTime ShiftStart { get; set; }
+        public DateTime ShiftEnd { get; set; }
+
+        public Shift(int shiftID, string position, DateTime shiftStart, DateTime shiftEnd)
+        {
+            ShiftID = shiftID;
+            Position = position;
+            ShiftStart = shiftStart;
+            ShiftEnd = shiftEnd;
+        }
+
+        public Shift(int shiftID, Employee employeeOnShift, string position, DateTime shiftStart, DateTime shiftEnd)
+        {
+            ShiftID = shiftID;
+            EmployeeOnShift = employeeOnShift;
+            Position = position;
+            ShiftStart = shiftStart;
+            ShiftEnd = shiftEnd;
+        }
+
+        public override string ToString()
+        {
+            if (EmployeeOnShift != null)
+            {
+                return $"{EmployeeOnShift} - {Position}: {ShiftStart} - {ShiftEnd}";
+            }
+            else
+            {
+                return $"{Position}: {ShiftStart} - {ShiftEnd}";
+
+            }
+        }
+    }
+}
